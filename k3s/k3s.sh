ed891781c17803d29c656732b8cdc4f7b1187a14
Cemal Y. Dalar <cemal@dalar.net>
2024-01-06 00:21:46 +0100

    fix cilium version

--- k3s/k3s.sh
+++ k3s/k3s.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ufw disable
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -s - --disable-network-policy --write-kubeconfig-mode=644 --flannel-backend none --token 123456
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -s - --disable=traefik --disable-network-policy --write-kubeconfig-mode=644 --flannel-backend none --token 123456
 # Check for Ready node, takes ~30 seconds 
 #sudo k3s kubectl get node 
 
@@ -17,5 +17,6 @@ export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 grep -qxF 'export KUBECONFIG=/etc/rancher/k3s/k3s.yaml' ~/.bashrc || echo 'export KUBECONFIG=/etc/rancher/k3s/k3s.yaml' >> ~/.bashrc
 
 # install cilium
-cilium install --version $CILIUM_CLI_VERSION
+CILIUM_STABLE_VERSION=$(cilium version --client | grep stable | awk '{print $4}')
+cilium install --version $CILIUM_STABLE_VERSION
 cilium status --wait
