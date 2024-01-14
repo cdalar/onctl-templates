5b8892223ea049eb2fc4f74fc05377e418335ac0
Cemal Y. Dalar <cemal@dalar.net>
2024-01-14 04:12:32 +0100

    docker for coder

--- coder/init.sh
+++ coder/init.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# install docker
+curl https://templates.onctl.com/docker/docker.sh | bash
+useradd -m -G docker ubuntu
+usermod -aG docker coder
 curl -L https://coder.com/install.sh | sh
 
 sudo systemctl enable --now coder
