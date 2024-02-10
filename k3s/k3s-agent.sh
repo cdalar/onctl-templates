3280640e87718e5c0a9487d17321aa3ab7404ae4
Cemal Y. Dalar <cemal@dalar.net>
2024-02-10 21:10:50 +0100

    randomize token

--- k3s/k3s-agent.sh
+++ k3s/k3s-agent.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://${SERVER_IP}:6443 --token 123456" sh -s -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://${SERVER_IP}:6443 --token ${TOKEN}" sh -s -
--- k3s/k3s-server.sh
+++ k3s/k3s-server.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 ufw disable
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -s - --disable-network-policy --write-kubeconfig-mode=644 --token 123456
+TOKEN=$(echo $RANDOM | md5sum | head -c 32)
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -s - --disable-network-policy --write-kubeconfig-mode=644 --token $TOKEN
 
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 grep -qxF 'export KUBECONFIG=/etc/rancher/k3s/k3s.yaml' ~/.bashrc || echo 'export KUBECONFIG=/etc/rancher/k3s/k3s.yaml' >> ~/.bashrc
 grep -qxF 'alias k=kubectl' ~/.bashrc || echo 'alias k=kubectl' >> ~/.bashrc
 cp /etc/rancher/k3s/k3s.yaml /tmp/k3s.yaml
 sed -i "s/127.0.0.1/${PUBLIC_IP}/g" /tmp/k3s.yaml
+echo $TOKEN > /tmp/token
