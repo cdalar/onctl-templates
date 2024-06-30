edbb18422411e0922f782de9f4892b118e7ea090
Cemal Y. Dalar <cemal@dalar.net>
2024-06-30 19:21:04 +0200

    pull llama3

--- ollama/open-webui-ollama.sh
+++ ollama/open-webui-ollama.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # install docker
 curl https://templates.onctl.com/docker/docker.sh | bash
+
 docker run -d -p 3000:8080 -v ollama:/root/.ollama -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:ollama
+docker exec open-webui ollama pull llama3
