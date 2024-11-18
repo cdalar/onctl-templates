10bf6d4e39429e2c9cbd1ca836cdb75277a66c10
Cemal Y. Dalar <cemal@dalar.net>
2024-11-18 08:05:38 +0100

    prometheus demo

---
+++ prometheus/prometheus.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# install docker
+curl https://templates.onctl.com/docker/docker.sh | bash
+
+cat <<EOF > prometheus.yml
+global:
+  scrape_interval: 10s
+scrape_configs:
+  - job_name: myvm
+    metrics_path: /metrics
+    static_configs:
+      - targets: [ 'localhost:9100' ]
+EOF
+cat <<EOF > docker-compose.yml
+services:
+  prometheus:
+    image: prom/prometheus:v2.34.0
+    container_name: prometheus
+    network_mode: "host"
+    ports:
+      - "9090:9090"
+    volumes:
+      - ${PWD}/prometheus.yml:/etc/prometheus/prometheus.yml
+  grafana:
+    image: grafana/grafana:8.4.4-ubuntu
+    container_name: grafana
+    network_mode: "host"
+    ports:
+      - "3000:3000"
+  node_exporter:
+    image: quay.io/prometheus/node-exporter:latest
+    container_name: node_exporter
+    command:
+      - '--path.rootfs=/host'
+    network_mode: host
+    pid: host
+    restart: unless-stopped
+    volumes:
+      - '/:/host:ro,rslave'
+EOF
+
+docker compose up -d 
