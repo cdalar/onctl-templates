01fb8b830b23fcb3b6dbeb3ba46ced591669abef
Cemal Y. Dalar <cemal@dalar.net>
2025-01-07 21:58:24 +0100

    node install

---
+++ node/node.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Download and install nvm:
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+
+. ~/.nvm/nvm.sh
+
+# Download and install Node.js:
+nvm install 22
+
+# Verify the Node.js version:
+node -v # Should print "v22.12.0".
+nvm current # Should print "v22.12.0".
+
+# Verify npm version:
+npm -v # Should print "10.9.0".
