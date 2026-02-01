1b9a3e02fcf6306a60754ff9d54cab929c40bcf5
Cemal Y. Dalar <cemal@dalar.net>
2026-02-01 09:14:21 +0100

    Add CSI driver deployment to k3s-ccm script

--- hcloud/k3s-ccm.sh
+++ hcloud/k3s-ccm.sh
@@ -4,3 +4,4 @@ kubectl -n kube-system create secret generic hcloud --from-literal=token="${HCLO
 # helm repo update hcloud
 # helm install hccm hcloud/hcloud-cloud-controller-manager -n kube-system
 kubectl apply -f https://raw.githubusercontent.com/hetznercloud/hcloud-cloud-controller-manager/refs/heads/main/deploy/ccm.yaml
+kubectl apply -f https://raw.githubusercontent.com/hetznercloud/csi-driver/v2.5.1/deploy/kubernetes/hcloud-csi.yml
